ebe8745b15092c77b441e8536d7738d947b0b8d2
kimdayday <kdh39278@gmail.com>
2024-11-12 17:02:28 +0900

    fix: change swaggerConfig which is for deploy

--- src/main/java/com/multitap/payment/common/config/SwaggerConfig.java
+++ src/main/java/com/multitap/payment/common/config/SwaggerConfig.java
@@ -1,24 +1,42 @@
 package com.multitap.payment.common.config;
 
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.servers.Server;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
 // 배포용
 @Configuration
 public class SwaggerConfig {
+
+    private static final String BEARER_TOKEN_PREFIX = "Bearer";
+
     @Bean
     public OpenAPI openAPI() {
+        String securityJwtName = "JWT";
+        SecurityRequirement securityRequirement = new SecurityRequirement().addList(
+            securityJwtName);
+        Components components = new Components()
+            .addSecuritySchemes(securityJwtName, new SecurityScheme()
+                .name(securityJwtName)
+                .type(SecurityScheme.Type.HTTP)
+                .scheme(BEARER_TOKEN_PREFIX)
+                .bearerFormat(securityJwtName));
         return new OpenAPI()
-            .components(new Components())
+            .addSecurityItem(securityRequirement)
+            .components(components)
+            .addServersItem(new Server().url("/payment-service"))
             .info(apiInfo());
     }
 
     private Info apiInfo() {
         return new Info()
-            .title("Payment-Service") // API의 제목
-            .description("Payment-Service Swagger UI") // API에 대한 설명
-            .version("v1"); // API의 버전
+            .title("PAYMENT SERVICE")
+            .description("PAYMENT SERVICE Swagger UI")
+            .version("1.0.0");
     }
 }
