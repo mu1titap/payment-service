16664920ef1c04f4dc33c4847e65f39879d1015c
kimdayday <kdh39278@gmail.com>
2024-11-13 12:04:16 +0900

    refactor: add BaseResponse to Controller

--- src/main/java/com/multitap/payment/api/presentation/PaymentController.java
+++ src/main/java/com/multitap/payment/api/presentation/PaymentController.java
@@ -7,6 +7,7 @@ import com.multitap.payment.api.dto.out.KakaoPayApproveResponseDto;
 import com.multitap.payment.api.vo.KakaoPayApproveRequestVo;
 import com.multitap.payment.api.vo.KakaoPayRequestVo;
 import com.multitap.payment.api.vo.KakaoPayResponseVo;
+import com.multitap.payment.common.entity.BaseResponse;
 import io.swagger.v3.oas.annotations.Operation;
 import lombok.AllArgsConstructor;
 import org.springframework.transaction.annotation.Transactional;
@@ -24,20 +25,21 @@ public class PaymentController {
 
     @PostMapping("/ready")
     @Operation(summary = "결제 준비 요청")
-    public KakaoPayResponseVo paymentReady(@RequestBody KakaoPayRequestVo vo){
+    public BaseResponse<KakaoPayResponseVo> paymentReady(@RequestBody KakaoPayRequestVo vo){
         KakaoPayRequestDto kakaoPayRequestDto = KakaoPayRequestDto.from(vo);
         // 결제 요청
-        return kakaoPayService.kakaoPayReady(kakaoPayRequestDto).toVo();
+        return new BaseResponse<>(kakaoPayService.kakaoPayReady(kakaoPayRequestDto).toVo());
     }
 
     @Operation(summary = "결제 승인 요청")
     @PostMapping("/approve")
-    public KakaoPayApproveResponseDto paymentApprove(@RequestBody KakaoPayApproveRequestVo kakaoPayApproveRequestVo){
+    public BaseResponse<KakaoPayApproveResponseDto> paymentApprove(@RequestBody KakaoPayApproveRequestVo kakaoPayApproveRequestVo){
         KakaoPayApproveResponseDto kakaoPayApproveResponseDto =
             kakaoPayService.kakaoPayApprove(KakaoPayApproveRequestDto.from(kakaoPayApproveRequestVo));
         // DB 저장
         kakaoPayService.createKakaoPay(kakaoPayApproveResponseDto);
-        return kakaoPayApproveResponseDto;
+
+        return new BaseResponse<>(kakaoPayApproveResponseDto);
 
     }
 
