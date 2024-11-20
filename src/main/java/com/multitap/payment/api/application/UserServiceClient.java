86fb3dcb22c3727c26583bc1cf8049cd495535f4
kimdayday <kdh39278@gmail.com>
2024-11-20 16:14:55 +0900

    refactor: delete url in feignClient

--- src/main/java/com/multitap/payment/api/application/UserServiceClient.java
+++ src/main/java/com/multitap/payment/api/application/UserServiceClient.java
@@ -7,11 +7,9 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.PostMapping;
 
-// ToDo discoveryService 이용해서 host url 가져오기
-// TODO pull 받은 다음 localhost에서 테스트 하기
+
 @Service
-@FeignClient(name = "user-service", url = "http://localhost:3929", configuration = UserServiceClientConfig.class)
-// url : test
+@FeignClient(name = "user-service", configuration = UserServiceClientConfig.class)
 public interface UserServiceClient {
 
     @PostMapping("api/v1/member/points/add")
