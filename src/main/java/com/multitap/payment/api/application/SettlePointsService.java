cca8835b9925443e692152a9a44c5edffaf57fdb
DaeHee Kim <96685431+everydayday@users.noreply.github.com>
2024-11-30 14:45:25 +0900

    feat: send Email
    # Conflicts:
    #	build.gradle
    #	src/main/java/com/multitap/payment/api/application/MemberVoltAmountServiceImpl.java
    #	src/main/java/com/multitap/payment/api/application/SettlePointsService.java
    #	src/main/java/com/multitap/payment/api/application/SettlePointsServiceImpl.java
    #	src/main/java/com/multitap/payment/api/presentation/PaymentController.java

---
+++ src/main/java/com/multitap/payment/api/application/SettlePointsService.java
@@ -0,0 +1,12 @@
+package com.multitap.payment.api.application;
+
+import com.multitap.payment.api.dto.in.SettlePointsDto;
+
+public interface SettlePointsService {
+
+    Boolean settlePoints(SettlePointsDto settlePointsDto);
+
+    void sendRandomNumber(String phoneNumber);
+
+    Boolean checkRandomNumber(String insertNumber, String phoneNumber);
+}
