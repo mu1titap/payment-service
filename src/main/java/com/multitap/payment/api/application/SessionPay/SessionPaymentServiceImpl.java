2e8911c0307f028fac64791ad69b0fdd9e98f153
DaeHee Kim <96685431+everydayday@users.noreply.github.com>
2024-12-09 16:01:29 +0900

    feat: add KafkaProducer

--- src/main/java/com/multitap/payment/api/application/SessionPay/SessionPaymentServiceImpl.java
+++ src/main/java/com/multitap/payment/api/application/SessionPay/SessionPaymentServiceImpl.java
@@ -1,8 +1,10 @@
 package com.multitap.payment.api.application.SessionPay;
 
 import com.multitap.payment.api.application.UserServiceClient;
+import com.multitap.payment.api.application.kafka.KafkaProducer;
 import com.multitap.payment.api.dto.in.SessionPaymentDto;
 import com.multitap.payment.api.dto.in.UserReqDto;
+import com.multitap.payment.api.dto.out.PaySessionRequestDto;
 import com.multitap.payment.api.infrastructure.VoltHistoryRepository;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -15,6 +17,7 @@ public class SessionPaymentServiceImpl implements SessionPaymentService {
 
     private final UserServiceClient userServiceClient;
     private final VoltHistoryRepository voltHistoryRepository;
+    private final KafkaProducer kafkaProducer;
 
     @Override
     public void paySession(SessionPaymentDto sessionPaymentDto) {
@@ -33,6 +36,14 @@ public class SessionPaymentServiceImpl implements SessionPaymentService {
         // 결제 정보 저장
         log.info("sessionPaymentDto : {}", sessionPaymentDto.toString());
         voltHistoryRepository.save(sessionPaymentDto.toEntity());
+
+        // kafka topic 발행
+        kafkaProducer.sendPaySessionMessage("pay-session", PaySessionRequestDto.builder()
+            .sessionUuid(sessionPaymentDto.getSessionUuid())
+            .menteeUuid(sessionPaymentDto.getMenteeUuid())
+            .mentorUuid(sessionPaymentDto.getMentorUuid())
+            .volt(sessionPaymentDto.getVolt())
+            .build());
 
 
     }
