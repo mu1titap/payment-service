b0f04c6e8a8a194ed541c2082f6a2c7dc3203851
DaeHee Kim <96685431+everydayday@users.noreply.github.com>
2024-12-09 14:17:47 +0900

    fix: add transactional annotation on the service interface

--- src/main/java/com/multitap/payment/api/application/PointHistoryPay/PointHistoryService.java
+++ src/main/java/com/multitap/payment/api/application/PointHistoryPay/PointHistoryService.java
@@ -2,7 +2,9 @@ package com.multitap.payment.api.application.PointHistoryPay;
 
 import com.multitap.payment.api.dto.out.PointHistoryResponseDto;
 import java.util.List;
+import org.springframework.transaction.annotation.Transactional;
 
+@Transactional(readOnly = true)
 public interface PointHistoryService {
 
     List<PointHistoryResponseDto> getPointHistory(String menteeUuid);
