02cc41e9d70efeb71f3f0d6a86cdf3e01c4b85b7
DaeHee Kim <96685431+everydayday@users.noreply.github.com>
2024-12-10 19:02:26 +0900

    temp

--- src/main/java/com/multitap/payment/api/infrastructure/PaymentInfoRepository.java
+++ src/main/java/com/multitap/payment/api/infrastructure/PaymentInfoRepository.java
@@ -3,7 +3,6 @@ package com.multitap.payment.api.infrastructure;
 import com.multitap.payment.api.domain.PaymentInfoEntity;
 import com.multitap.payment.api.dto.out.PaymentResponseDto;
 import java.util.List;
-import java.util.Optional;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -12,8 +11,7 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface PaymentInfoRepository extends JpaRepository<PaymentInfoEntity, Long> {
-
-    Optional<List<PaymentInfoEntity>> findByMenteeUuid(String menteeUuid);
+    
 
     @Query(name = "payment_info_dto", nativeQuery = true)
     List<PaymentResponseDto> getPaymentInfo(@Param("menteeUuid") String menteeUuid,
