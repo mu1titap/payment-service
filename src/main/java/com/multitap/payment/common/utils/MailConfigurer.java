ba456a102207f2878c007bee2c9b7cf579e0f2af
DaeHee Kim <96685431+everydayday@users.noreply.github.com>
2024-12-11 21:21:40 +0900

    fix: fix to enviromental variable

--- src/main/java/com/multitap/payment/common/utils/MailConfigurer.java
+++ src/main/java/com/multitap/payment/common/utils/MailConfigurer.java
@@ -10,7 +10,7 @@ public class MailConfigurer {
 
         try {
             message.setFrom(senderEmail);
-            message.setRecipients(MimeMessage.RecipientType.TO, "kdh39278@naver.com");
+            message.setRecipients(MimeMessage.RecipientType.TO, toEmail);
             message.setSubject("이메일 인증");
             String body = "";
             body += "<h3>" + "요청하신 인증 번호입니다." + "</h3>";
