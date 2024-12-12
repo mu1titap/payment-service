ebcc39a9772527f6c1ff2496e3444bd9617695bd
DaeHee Kim <96685431+everydayday@users.noreply.github.com>
2024-12-12 18:56:45 +0900

    comment

--- src/main/java/com/multitap/payment/api/application/kafka/KafkaProducerConfig.java
+++ src/main/java/com/multitap/payment/api/application/kafka/KafkaProducerConfig.java
@@ -28,7 +28,7 @@ public class KafkaProducerConfig {
         configProps.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
         configProps.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
         return new DefaultKafkaProducerFactory<>(configProps);
-    }
+    }   // tets
 
     @Bean
     public KafkaTemplate<String, PaySessionRequestDto> kafkaPaySessionTemplate() {
